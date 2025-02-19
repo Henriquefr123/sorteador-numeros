abd2d41df6f42a258dbc5111f6fd39d527921da9
Henrique Gabriel dos Santos Brandão <henriquefr321@gmail.com>
2025-02-19 11:33:03 -0300

    commit
    adicionando os arquivos
    19/02

---
+++ app.js
@@ -0,0 +1,14 @@
+function sortear() {
+    let quantidade = document.getElementById('quantidade').value;
+    let de = document.getElementById('de').value;
+    let ate = document.getElementById('ate').value;
+
+    console.log(`quantidade: ${quantidade}`)
+    console.log(`de: ${de}`)
+    console.log(`ate: ${ate}`)
+
+}
+
+// sortear()
+
+// function reiniciar () {}
